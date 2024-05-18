c48fc34fd950d148f7e088cf514fd3fd2612d243
ImNotSkidoLOL <pavel.y.smirnov@yandex.ru>
2024-05-18 22:39:46 +0300

    Add files via upload

---
+++ Baft Farm/Instances for script/AntiKick.lua
@@ -0,0 +1,43 @@
+-- // Only allow to be executed once
+assert(not getgenv().AntiKickExecuted, "Anti Kick is already running!")
+
+-- // Services
+local Players = game:GetService("Players")
+
+-- // Vars
+local LocalPlayer = Players.LocalPlayer
+local KickAttemptFormat = "Kick attempted for: %s"
+local BlockedMethods = {"Kick", "kick"}
+
+-- // What to do when Kick detected
+local function kickDetected(Player, Reason)
+    warn(KickAttemptFormat:format(Reason))
+end
+
+-- // Index hook
+hookfunction(LocalPlayer.Kick, kickDetected)
+
+-- // Namecall hook
+local __namecall
+__namecall = hookmetamethod(game, "__namecall", function(self, ...)
+    -- // Vars
+    local args = {...}
+    local method = getnamecallmethod()
+    
+    -- // Check if kick attempt
+    if (self == LocalPlayer and table.find(BlockedMethods, method)) then
+        -- // Alert
+        kickDetected(self, args[1])
+        
+        -- // Cancel it
+        return
+    end
+    
+    -- // Return - so everything else that uses namecall does not break
+    return __namecall(self, ...)
+end)
+
+-- // Credits
+getgenv().AntiKickExecuted = true
+messagebox("Loaded.\n\n\n- Created By Lazy as fuvk qqwizzixxxx", "Anti Kick", 0)
+
